6db095d0ad2f5338d18470cb7f284033c60a95a7
Matthew Hunter <matthew@innostream.co>
2022-06-21 22:11:17 +0200

    ADD PrintSomething

---
+++ bad.go
@@ -0,0 +1,18 @@
+package bad_swagger_tags_test
+
+import "fmt"
+
+//
+// Computes the set of conflicting or ambiguous alternatives from a
+// configuration set, if that information was not already provided by the
+// parser.
+//
+// @param ReportedAlts The set of conflicting or ambiguous alternatives, as
+// Reported by the parser.
+// @param configs The conflicting or ambiguous configuration set.
+// @return Returns {@code ReportedAlts} if it is not {@code nil}, otherwise
+// returns the set of alternatives represented in {@code configs}.
+//
+func PrintSomething() {
+	fmt.Println("Something")
+}
